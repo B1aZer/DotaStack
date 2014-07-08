7b39ac7c0afd2149cfad664ecbfb50cca24407f4
Dmitry Branitskiy <dmitry.branitskiy@gmail.com>
2014-07-08 18:21:16 +0400

    removing _ from heroes tinymce plugin

--- theme/static/grappelli/tinymce/jscripts/tiny_mce/plugins/heroes/js/heroes.js
+++ theme/static/grappelli/tinymce/jscripts/tiny_mce/plugins/heroes/js/heroes.js
@@ -2,7 +2,7 @@ function gotoherolink(a) {
   var href = a.getAttribute('href');
   var last_pos = href.length;
   var pos = href.lastIndexOf("/", last_pos - 2);
-  var name = href.substring(pos + 1, last_pos - 1)
+  var name = href.substring(pos + 1, last_pos - 1).replace('_',' ');
   var smallImg = a.children[1];
   var item = {img: smallImg.getAttribute('src'), href:href, dname:name};
   strHTML = '';
